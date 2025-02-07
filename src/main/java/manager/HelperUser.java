a6f55c523905542a67fa6246f0b60b3855a56a1a
Ta25Li <2885921@gmail.com>
2025-02-07 18:55:40 +0200

    added login success

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,9 +1,35 @@
 package manager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HelperUser extends HelperBase{
     public HelperUser(WebDriver wd) {
         super(wd);
     }
+    public void openLogRegForm() {
+   click(By.cssSelector("a[ng-reflect-router-link='login']"));
+    }
+
+    public void fillLogRegForm(String email, String password) {
+ type(By.xpath("//input[@id='email']"), email);
+ type(By.xpath("//input[@id='password']"), password);
+    }
+
+    public void submitLogin(){ //find btn Login and click
+        click(By.xpath("//button[@type='submit']"));
+    }
+
+    public boolean isLogged() {
+        return isElementPresent(By.xpath("//h2[normalize-space()='Logged in success']"));
+    }
+public void okClick(){
+    click(By.xpath("//button[@type='button']"));
+}
+//public void clickLogout(){
+ //   click(By.xpath("//a[normalize-space()='Logout']"));
+//}
+   public void logout() {
+       click(By.xpath("//a[normalize-space()='Logout']"));
+  }
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,11 +1,39 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase{
 
+  @BeforeMethod
+   public void preCondition() {
+       //if SignOut present --> logout
+        if (app.getHelperUser().isLogged())
+            app.getHelperUser().logout();
+    }
+        @Test
+        public void loginSuccess() {
+            app.getHelperUser().openLogRegForm();
+            app.getHelperUser().fillLogRegForm("leya@bach.com", "leyaBach9!");
+            app.getHelperUser().submitLogin();
+
+            Assert.assertTrue(app.getHelperUser().isLogged());
+
+            app.getHelperUser().okClick();
+
+        }
+
     @Test
-    public void test(){
+    public void loginSuccessModel() {
+        app.getHelperUser().openLogRegForm();
+        app.getHelperUser().fillLogRegForm("leya@bach.com", "leyaBach9!");
+        app.getHelperUser().submitLogin();
+
+        Assert.assertTrue(app.getHelperUser().isLogged());
+
+        app.getHelperUser().okClick();
 
     }
-}
+    }
+
