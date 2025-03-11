bf8bcbc23327bb491bbc39a2e33c810665fa9b0f
Ta25Li <2885921@gmail.com>
2025-03-11 18:11:34 +0200

    add wrong dates

--- src/main/java/manager/HelperCar.java
+++ src/main/java/manager/HelperCar.java
@@ -174,4 +174,13 @@ public class HelperCar extends HelperBase {
     public void navigateByLogo() {
         click(By.cssSelector("a.logo"));
     }
+
+    public void searchNotValidPeriod(String city, String dateFrom, String dateTo) {
+        typeCity(city);
+        clearTextBox(By.id("dates"));
+        type(By.id("dates"), dateFrom+" - "+ dateTo);
+        click(By.cssSelector("div.cdk-overlay-backdrop")); //click any place on screen to close calendar
+
+
+    }
 }
--- src/test/java/tests/SearchCar.java
+++ src/test/java/tests/SearchCar.java
@@ -8,7 +8,7 @@ public class SearchCar extends TestBase {
 
     @Test
     public void searchCurrentMonthSuccess(){
-        app.getHelperCar().searchCurrentMonth("Rehovot","10/10/2025","10/27/2025");
+        app.getHelperCar().searchCurrentMonth("Rehovot","03/20/2025","03/27/2025");
         app.getHelperCar().getScreen("src/test/screenshots/currentMth.png");
         app.getHelperCar().submit();
 
@@ -34,6 +34,16 @@ public class SearchCar extends TestBase {
         Assert.assertTrue(app.getHelperCar().isCarListAppeared());
 
     }
+
+    @Test
+    public void negativeSearch(){
+        app.getHelperCar().searchNotValidPeriod("Rehovot","2/15/2025","2/10/2026");
+        app.getHelperCar().submit();
+        Assert.assertTrue(app.getHelperUser().isYallaBtnNotActive());
+        Assert.assertTrue(app.getHelperUser().getErorrText().contains("You can't pick date before today"));
+
+    }
+
     @AfterMethod
     public void postCondition(){
         app.getHelperCar().navigateByLogo();
